a608b0461b36c9e5af1a09c5aa0f13ed3454c446
Irene Chen <irene85201@gmail.com>
2021-04-19 15:47:42 +0800

    add member edit

--- GitSampleTest/src/GitTest/GGGGGGGGGGIT.java
+++ GitSampleTest/src/GitTest/GGGGGGGGGGIT.java
@@ -5,6 +5,8 @@ public class GGGGGGGGGGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1Vres");
+		//irene85201 edit
+		System.out.println("member edit");
 	}
 
 }
